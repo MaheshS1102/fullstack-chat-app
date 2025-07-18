0fa6b8edb1e4b6e12361bfbd2c99d4ed6f5a1b17
Mahesh Salunke <maheshgsal76@gmail.com>
2025-07-18 22:06:34 +0530

    Updated backend with latest changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,6 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
-import { fileURLToPath } from "url";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,18 +13,14 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-// Fix __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(
   cors({
-    origin: "http://localhost:5173", // or your frontend deployed URL
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
@@ -33,15 +28,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// ✅ FIXED: Serve frontend from correct absolute path
-// if (process.env.NODE_ENV === "production") {
-//   const frontendPath = path.resolve(__dirname, "../../frontend/dist");
-//   app.use(express.static(frontendPath));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(frontendPath, "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,9 +3,10 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
-router.post("/send/:id", protectRoute, sendMessage);     // POST
-router.get("/users", protectRoute, getUsersForSidebar);  // GET users
-router.get("/:id", protectRoute, getMessages);           // GET messages (dynamic)
 
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
